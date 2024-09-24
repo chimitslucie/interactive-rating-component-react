0f249257f42452c4cf7b98b3a2c31e068b051a18
chimitslucie <chimitslucie@gmail.com>
2024-09-24 10:03:46 +0200

    modification classname

--- src/Components/CardRating.jsx
+++ src/Components/CardRating.jsx
@@ -3,25 +3,25 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function CardRating() {
     return (
-        <div className="cardContent">
-            <div className="cardContentIcon">
-                <FontAwesomeIcon icon={faStar} className="cardContentIconStar" />
+        <div className="cardRatingContent">
+            <div className="cardRatingContentIcon">
+                <FontAwesomeIcon icon={faStar} className="cardRatingContentIconStar" />
             </div>
-            <h1 className="cardContentTitle">
+            <h1 className="cardRatingContentTitle">
                 How did we do?
             </h1>
-            <p className="cardContentText">
+            <p className="cardRatingContentText">
                 Please let us know how we did with your support request. All feedback is appreciated
                 to help us improve our offering!
             </p>
-            <div className="cardContentRating">
-                <button className="cardContentRatingBtn">1</button>
-                <button className="cardContentRatingBtn">2</button>
-                <button className="cardContentRatingBtn">3</button>
-                <button className="cardContentRatingBtn">4</button>
-                <button className="cardContentRatingBtn">5</button>
+            <div className="cardRatingContentRating">
+                <button className="cardRatingContentRatingBtn">1</button>
+                <button className="cardRatingContentRatingBtn">2</button>
+                <button className="cardRatingContentRatingBtn">3</button>
+                <button className="cardRatingContentRatingBtn">4</button>
+                <button className="cardRatingContentRatingBtn">5</button>
             </div>
-            <button className="cardContentBtn">submit</button>
+            <button className="cardRatingContentBtn">submit</button>
         </div>
     );
 }
